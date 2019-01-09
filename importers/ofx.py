997b51b3f97704b15879d8198ffd6604c1829f59
Zahan Malkani <zahanm@fb.com>
2019-01-09 01:32:38 -0800

    Made my own OFX importer
    Relies on `ofxparse` and does a better job with the payee and such
    And can be customized more, of course

---
+++ importers/ofx.py
@@ -0,0 +1,84 @@
+
+from ofxparse import OfxParser
+
+from beancount.core.number import D
+from beancount.core.amount import Amount
+from beancount.core import data
+from beancount.ingest import importer
+
+from datetime import timedelta
+from io import StringIO
+from os import path
+
+class Importer(importer.ImporterProtocol):
+    """An importer for Open Financial Exchange files."""
+
+    def __init__(self, account_id, account, currency):
+        """Create a new importer posting to the given account."""
+        self.account_id = account_id
+        self.account = account
+        self.currency = currency
+
+    def name(self):
+        """Include the filing account in the name."""
+        return '{}: "{}"'.format(super().name(), self.file_account(None))
+
+    def identify(self, file):
+        # Match for a compatible MIME type.
+        if file.mimetype() not in {'application/x-ofx',
+                                   'application/vnd.intu.qbo',
+                                   'application/vnd.intu.qfx'}:
+            return False
+
+        # Match the account id.
+        ofx = OfxParser.parse(strio(file.contents()))
+        return ofx.account.account_id == self.account_id
+
+    def file_account(self, _):
+        """Return the account against which we post transactions."""
+        return self.account
+
+    def file_date(self, file):
+        ofx = OfxParser.parse(strio(file.contents()))
+        return ofx.account.statement.end_date
+
+    def extract(self, file, existing_entries=None):
+        """Extract a list of partially complete transactions from the file."""
+        return extract(file, self.account, self.FLAG, self.currency)
+
+
+def extract(file, account_name, flag, currency):
+    ofx = OfxParser.parse(strio(file.contents()))
+    account = ofx.account
+    statement = account.statement
+    assert statement.currency.lower() == currency.lower(), statement.currency + ' != ' + currency
+    ledger = []
+    # create transactions
+    for transaction in statement.transactions:
+        units = Amount(transaction.amount, currency)
+        posting = data.Posting(account_name, units, None, None, None, None)
+        ref = data.new_metadata(file.name, 0)
+        entry = data.Transaction(ref, transaction.date.date(), flag, transaction.payee, transaction.memo,
+            data.EMPTY_SET, data.EMPTY_SET, [posting])
+        ledger.append(entry)
+    ledger = data.sorted(ledger)
+    # make balance
+    ledger.append(balance(file, account_name, currency, statement))
+    return ledger
+
+
+def balance(file, account_name, currency, statement):
+    # Use the last transaction date as the balance assertion date
+    # (because the pending transactions will post in-between)
+    assert len(statement.transactions) > 0
+    date = statement.transactions[-1].date.date()
+    # The Balance assertion occurs at the beginning of the date, so move
+    # it to the following day.
+    date += timedelta(days=1)
+    units = Amount(statement.balance, currency)
+    ref = data.new_metadata(file.name, 0)
+    return data.Balance(ref, date, account_name, units, None, None)
+
+
+def strio(s):
+    return StringIO(s)
--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import sys
 from os import path
 sys.path.insert(0, path.join(path.dirname(__file__)))
 
-from importers import firstpub, amex
+from importers import ofx
 import collector
 
 from beancount.ingest import extract
-from beancount.ingest.importers import ofx
 from beancount.ingest.scripts_utils import ingest
 import yaml
 
@@ -29,11 +28,8 @@ def make_importer(account):
     return ofx.Importer(
         account['account-id'],
         account['name'],
-        balance_type=ofx.BalanceType.LAST)
+        account['currency'])
 
 importers = map(make_importer, CONFIG.values())
-
-# Override the header on extracted text (if desired).
-extract.HEADER = ';; -*- mode: org; mode: beancount; coding: utf-8; -*-\n'
 
 ingest(importers)
