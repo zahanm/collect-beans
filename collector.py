50acf4ce445d012099b18fc53f05389b60241447
Zahan Malkani <zahanm@fb.com>
2019-01-09 00:14:52 -0800

    Success! Used the provided ofx importer
    It's not too great though. Doesn't ever try to get a payee and spams the
    narraction with transaction IDs and such. I may build my own importer in
    the next commit

--- collector.py
+++ collector.py
@@ -5,23 +5,18 @@ from ofxhome import OFXHome
 from os import path
 import shutil
 import sys
-import yaml
+from typing import Any, Dict
 
-def run():
+def run(CONFIG: Dict[str, Any]):
   parser = argparse.ArgumentParser(description="Download statements from banks")
   parser.add_argument('--days', type=int, default=5, help='How many days back should the statement go?')
   parser.add_argument('--out', '-o', required=True, help='Which folder to store the .ofx files in.')
   args = parser.parse_args(sys.argv[2:])
 
-  # load accounts config
-  fname = path.join(path.dirname(__file__), 'CONFIG.yaml')
-  with open(fname) as f:
-      CONFIG = yaml.load(f)
-
   # look up and download for each
   for name, importer in CONFIG.items():
       print('Importer:', name)
-      bank = OFXHome.lookup(importer['OFX-ID'])
+      bank = OFXHome.lookup(importer['OFX-id'])
       print('Bank:', bank.name)
       client = Institution(bank.fid, bank.org, bank.url, importer['username'], importer['pw'])
       assert len(client.accounts()) == 1
--- main.py
+++ main.py
@@ -9,20 +9,29 @@ from importers import firstpub, amex
 import collector
 
 from beancount.ingest import extract
-from beancount.ingest.importers import csv
+from beancount.ingest.importers import ofx
 from beancount.ingest.scripts_utils import ingest
+import yaml
 
+# load accounts config
+fname = path.join(path.dirname(__file__), 'CONFIG.yaml')
+with open(fname) as f:
+    CONFIG = yaml.load(f)
 
 # if this is the download flow
 if len(sys.argv) > 1 and sys.argv[1] == 'download':
-    collector.run()
+    collector.run(CONFIG)
     exit(0)
 
 # otherwise, go through the regular `bean-{identify,extract,file}` flow
 
-# Setting this variable provides a list of importer instances.
-importers = [
-]
+def make_importer(account):
+    return ofx.Importer(
+        account['account-id'],
+        account['name'],
+        balance_type=ofx.BalanceType.LAST)
+
+importers = map(make_importer, CONFIG.values())
 
 # Override the header on extracted text (if desired).
 extract.HEADER = ';; -*- mode: org; mode: beancount; coding: utf-8; -*-\n'
