53b1572fed2c42f25cc6e04f776adb48a46bf9e3
Zahan Malkani <zahanm@fb.com>
2019-01-08 17:41:28 -0800

    made a YAML file for config, using pipenv

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """Import configuration."""
 
 # Insert our custom importers path here.
@@ -11,35 +10,18 @@ from importers import firstpub, amex
 from beancount.ingest import extract
 from beancount.ingest.importers import csv
 from beancount.ingest.scripts_utils import ingest
+from os import path
+import yaml
+
+fname = path.join(path.dirname(__file__), 'CONFIG.yaml')
+with open(fname) as f:
+    CONFIG = yaml.load(f)
 
 # Setting this variable provides a list of importer instances.
-CONFIG = [
-    # FirstPub
-    firstpub.Importer({
-                    csv.Col.DATE: "Date",
-                    csv.Col.AMOUNT_DEBIT: "Debit",
-                    csv.Col.AMOUNT_CREDIT: "Credit",
-                    csv.Col.PAYEE: "Description"
-                },
-                "Assets:US:FirstPub:Checking",
-                "USD",
-                row_processor=firstpub.Processor()),
-
-    # AmEx Gold
-    csv.Importer({
-                csv.Col.DATE: 0,
-                csv.Col.AMOUNT: 7,
-                csv.Col.PAYEE: 11},
-                "Liabilities:US:AmEx:Gold",
-                "USD",
-                # regexp to look for in the file content
-                [
-                    ('filename', 'Transactions.csv')
-                ],
-                row_processor=amex.Processor())
+importers = [
 ]
 
 # Override the header on extracted text (if desired).
 extract.HEADER = ';; -*- mode: org; mode: beancount; coding: utf-8; -*-\n'
 
-ingest(CONFIG)
+ingest(importers)
