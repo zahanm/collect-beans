73202da57852baf7005f6fac86a26e8775d04718
Zahan Malkani <zahanm@fb.com>
2019-01-12 11:31:50 -0800

    Handle multiple accounts per credential
    Since a single bank can indeed have multiple accounts

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import collector
 from beancount.ingest import extract
 from beancount.ingest.scripts_utils import ingest
 
+from itertools import chain
 from os import path
 import sys
 import yaml
@@ -31,23 +32,27 @@ if "collect" in sys.argv:
 # otherwise, go through the regular `bean-{identify,extract,file}` flow
 
 
-def make_importer(item):
-    (name, account) = item
-    if account["importer"] == "OFX":
-        return ofx.Importer(account["name"], account["currency"], account["account-id"])
-    elif account["importer"] == "CSV":
-        return csv.Importer(
-            account["column_map"],
-            account["name"],
-            account["currency"],
-            content_regexp=account.get("content_regexp"),
-            filename_regexp=account.get("filename_regexp"),
-            file_prefix=name,
-        )
-    else:
-        assert False, "Invalid importer: " + repr(account)
+def make_importers(item):
+    (credentials_name, institution) = item
+
+    def importer(account):
+        if institution["importer"] == "OFX":
+            return ofx.Importer(account["name"], institution["currency"], account["id"])
+        elif institution["importer"] == "CSV":
+            return csv.Importer(
+                institution["column_map"],
+                account["name"],
+                institution["currency"],
+                content_regexp=institution.get("content_regexp"),
+                filename_regexp=institution.get("filename_regexp"),
+                file_prefix=credentials_name + account["id"],
+            )
+        else:
+            assert False, "Invalid importer: " + repr(institution) + " " + repr(account)
+
+    return map(importer, institution["accounts"])
 
 
-importers = map(make_importer, CONFIG.items())
+importers = chain.from_iterable(map(make_importers, CONFIG.items()))
 
 ingest(importers)
