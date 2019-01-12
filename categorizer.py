f01357d978a715701f23150bac8cbe62c4218231
Zahan Malkani <zahanm@fb.com>
2019-01-12 09:18:33 -0800

    It works! Can categorize simple transactions
    Just looking at payee names for now. That's like a 50% win for sure

--- categorizer.py
+++ categorizer.py
@@ -5,6 +5,25 @@ from beancount.core.amount import Amount
 from beancount.core.data import Transaction, Posting
 
 import argparse
+import re
+from typing import Optional
+
+
+straight_matches = {
+    r"Tartine Bakery": "Expenses:Food:Snacks",
+    r"Trader Joe's": "Expenses:Food:Groceries",
+}
+straight_regexs = dict(
+    zip((re.compile(pat) for pat in straight_matches), straight_matches.values())
+)
+
+
+def attempt_categorize(entry) -> Optional[str]:
+    for pat, account in straight_regexs.items():
+        if pat.search(entry.payee) != None:
+            return account
+    # random = Posting("Equity:Random", None, None, None, None, None)
+    return None
 
 
 def main():
@@ -17,8 +36,12 @@ def main():
         if not isinstance(entry, Transaction):
             new_entry = entry
         else:
-            random = Posting("Equity:Random", None, None, None, None, None)
-            new_postings = entry.postings + [random]
+            account = attempt_categorize(entry)
+            if account:
+                posting = Posting(account, None, None, None, None, None)
+                new_postings = entry.postings + [posting]
+            else:
+                new_postings = entry.postings
             new_entry = Transaction(
                 entry.meta,
                 entry.date,
