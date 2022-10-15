871abd6bd6bc478459afae6937f53080c07dbe3a
Zahan Malkani <z@zahanm.com>
2022-10-15 18:25:46 +0530

    A positional argument for operations to run

--- bookkeeper/README.md
+++ bookkeeper/README.md
@@ -1,8 +1,17 @@
-How to run this?
+# How to run this?
+
+Config
+
+- Needs the ledger files mounted at /data
+- Needs the source files (.py) mounted at /src
 
 ```
 docker build --tag bookkeeper .
-docker run --rm -it bookkeeper
+docker run --rm --volume "$(pwd)/../accounts:/data" --volume "$(pwd):/src" -it bookkeeper collect-and-so
+rt /data/CONFIG.yaml
 ```
 
-Note that the first and only argument to the script is the location of the `CONFIG.yaml` file.
+Note that there are two positional arguments:
+
+1. The operations to run
+1. The location of the `CONFIG.yaml` file.
--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -1,6 +1,6 @@
 import yaml
 
-import sys
+import argparse
 
 
 class Runner:
@@ -10,15 +10,41 @@ class Runner:
     Categorise = suggest an expense account for transactions from the ledger that are missing one.
     """
 
-    def __init__(self, config: str):
-        with open(config) as f:
+    def __init__(self):
+        args = self._extract_args()
+        print(args)
+        with open(args.config) as f:
             self.config = yaml.full_load(f)
+        self.run = args.func
 
-    def run(self):
-        print(sys.argv)
+    def collect_and_sort(self):
+        print("collect and sort")
         print(list(self.config["categories"].keys())[0])
+
+    def sort_only(self):
+        print("sort only")
+
+    def collect_only(self):
+        print("collect only")
+
+    def _extract_args(self):
+        parser = argparse.ArgumentParser(
+            description="Collect and categorise transations. Takes them from $source, categorises them, and writes them to $destination."
+        )
+        parser.add_argument("--debug", action="store_true", help="Debug the steps")
+        subp = parser.add_subparsers(help="Top level")
+        everything = subp.add_parser("collect-and-sort", help="Does everything")
+        everything.set_defaults(func=self.collect_and_sort)
+        sort = subp.add_parser("sort-only", help="Only does categorisation step")
+        sort.set_defaults(func=self.sort_only)
+        collect = subp.add_parser("collect-only", help="Only does collection step")
+        collect.set_defaults(func=self.collect_only)
+        parser.add_argument(
+            "config", help="YAML file with configuration for this collector"
+        )
+        return parser.parse_args()
 
 
 if __name__ == "__main__":
-    r = Runner(sys.argv[1])
+    r = Runner()
     r.run()
