ff1af9c7e13fa21280fb94f6694e86ebe852ba3a
Zahan Malkani <z@zahanm.com>
2022-10-17 22:26:08 +0530

    Add first real route: /get_progress
    Gives the .beancount files present in /data

--- bookkeeper/api/__init__.py
+++ bookkeeper/api/__init__.py
@@ -1,6 +1,8 @@
 import yaml
 from flask import Flask
 
+from pathlib import Path
+
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +15,15 @@ def create_app():
         return {
             "message": "Hello, World out there!",
             "test-config": list(config["categories"].keys())[0],
+        }
+
+    @app.route("/get_progress")
+    def progress():
+        data = Path("/data")
+        return {
+            "destination_file": None,
+            "main_file": None,
+            "journal_files": [p.name for p in data.glob("*.beancount")],
         }
 
     return app
