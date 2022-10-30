16ec1c8c615482b73ae42637ed29fd1f7c463970
Zahan Malkani <z@zahanm.com>
2022-10-30 16:44:59 +0530

    Render selects for the main and destination files

--- bookkeeper/web/src/SortOptions.tsx
+++ bookkeeper/web/src/SortOptions.tsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function SortOptions() {
+  const [journalFiles, setJournalFiles] = useState<Array<string>>([]);
+  const [mainFile, setMainFile] = useState<string>();
+  const [destFile, setDestFile] = useState<string>();
+
   useEffect(() => {
     const fetchData = async () => {
       const resp = await fetch("http://localhost:5005/progress", {
@@ -11,17 +15,71 @@ function SortOptions() {
       });
       const data = await resp.json();
       console.log(data);
+      setJournalFiles(data.journal_files);
     };
 
     fetchData().catch((err) => console.error(err));
-  });
+  }, []);
+  // The empty array of dependencies is important. It tells the
+  // effect to only run once.
 
   return (
-    <div>
-      <h2>Start Sort</h2>
-      <Link to={`/sort/choose`} className="text-sky-400">
-        Next
-      </Link>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h2 className="text-xl">Start Sort</h2>
+      <form
+        onSubmit={(ev) => {
+          ev.preventDefault();
+          console.log("submitting form", ev);
+        }}
+      >
+        <p className="py-1">
+          <label htmlFor="main_file" className="mr-1">
+            Main file:
+          </label>
+          <select
+            name="main_file"
+            value={mainFile}
+            onChange={(ev) => setMainFile(ev.target.value)}
+            required
+            className="text-black"
+          >
+            {journalFiles.map((f) => (
+              <option value={f} key={f}>
+                {f}
+              </option>
+            ))}
+          </select>
+        </p>
+        <p className="py-1">
+          <label htmlFor="dest_file" className="mr-1">
+            Destination file:
+          </label>
+          <select
+            name="dest_file"
+            value={destFile}
+            onChange={(ev) => setDestFile(ev.target.value)}
+            required
+            className="text-black"
+          >
+            {journalFiles.map((f) => (
+              <option value={f} key={f}>
+                {f}
+              </option>
+            ))}
+          </select>
+        </p>
+        <p className="py-1 text-center">
+          <button type="submit">Save</button>
+        </p>
+      </form>
+      <div>
+        <Link to={`/`} className="text-sky-400 mr-10">
+          Cancel
+        </Link>
+        <Link to={`/sort/choose`} className="text-sky-400">
+          Start
+        </Link>
+      </div>
     </div>
   );
 }
