8f3211fa506ea535dbc922beeb38dcc5718b0cd0
Zahan Malkani <z@zahanm.com>
2022-11-02 15:01:00 +0530

    Fixed the layout of the auto-complete. It works now!

--- bookkeeper/web/src/Transaction.tsx
+++ bookkeeper/web/src/Transaction.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import dayjs from "dayjs";
 import { Set } from "immutable";
-import { Combobox } from "@headlessui/react";
+import { Combobox, Transition } from "@headlessui/react";
 import {
   PaperAirplaneIcon,
   PlusCircleIcon,
   MinusCircleIcon,
+  CheckIcon,
+  ChevronUpDownIcon,
 } from "@heroicons/react/20/solid";
 
 import { IDirectiveForSort, IDirectiveMod, IPosting } from "./beanTypes";
@@ -184,15 +186,62 @@ function AccountSelector(props: {
             required
             onChange={(event) => setQuery(event.target.value)}
           />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronUpDownIcon
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </Combobox.Button>
         </div>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setQuery("")}
+        >
+          <Combobox.Options className="absolute mt-1 max-h-60 w-[48ch] z-10 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            {filteredAccounts.size === 0 && query !== "" ? (
+              <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                Nothing found.
+              </div>
+            ) : (
+              filteredAccounts.map((account) => (
+                <Combobox.Option
+                  key={account}
+                  value={account}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-teal-600 text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {account}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
+                          }`}
+                        >
+                          <CheckIcon className="h-5 w-5" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))
+            )}
+          </Combobox.Options>
+        </Transition>
       </div>
-      <Combobox.Options>
-        {filteredAccounts.map((account) => (
-          <Combobox.Option key={account} value={account}>
-            {account}
-          </Combobox.Option>
-        ))}
-      </Combobox.Options>
     </Combobox>
   );
 }
