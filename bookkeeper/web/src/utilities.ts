a1aed16a30d0b277237b1e3248d92490c3c3820b
Zahan Malkani <z@zahanm.com>
2022-11-07 12:41:27 -0800

    invariant() utility in frontend

--- bookkeeper/web/src/utilities.ts
+++ bookkeeper/web/src/utilities.ts
@@ -6,3 +6,16 @@ export const errorHandler = (err: Error) => {
 export function arrayRange(num: number): Array<number> {
   return [...Array(num).fill(0)].map((_, ii) => ii);
 }
+
+export function invariant(condition: boolean, message?: string) {
+  if (!condition) {
+    throw new InvariantViolationError(message || "Invariant failed");
+  }
+}
+
+class InvariantViolationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
