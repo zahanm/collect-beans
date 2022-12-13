915826aaf46cb2b54dd587a9499dd5892fec8d7b
Zahan Malkani <z@zahanm.com>
2022-12-13 15:45:19 -0800

    Removing an accidental import

--- bookkeeper/web/src/CollectOptions.tsx
+++ bookkeeper/web/src/CollectOptions.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { isPropertySignature } from "typescript";
 
 import { CollectMode } from "./beanTypes";
 import { API, errorHandler, invariant } from "./utilities";
